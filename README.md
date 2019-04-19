c930c0193aa8be2ad32da62933c24bc153ac828c
brant-ruan <414372820@qq.com>
2019-04-20 07:48:44 +0800

    add idfconsole.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IDF4APEV
+
 Integrated Detection Framework for Android's Privilege Escalation Vulnerabilites
---
+++ idfconsole.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@File    : idfconsole.py.py
+@Time    : 2019-04-20 07:47
+@Author  : Bonan Ruan
+@Desc    :
+"""
