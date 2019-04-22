1f7e0c29892e2c0758883e174b65b6327e45f090
brant-ruan <414372820@qq.com>
2019-04-23 00:11:28 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-@File    : __init__.py.py
+@File    : __init__.py
 @Time    : 2019-04-20 11:06
 @Author  : Bonan Ruan
 @Desc    :
--- utils/consts.py
+++ utils/consts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-@File    : consts.py.py
+@File    : consts.py
 @Time    : 2019-04-20 11:09
 @Author  : Bonan Ruan
 @Desc    :
